3bbb8e6c8fbb25bd64dbbf7218a5869be9f4a116
Damon Simpson <damon.a.simpson@gmail.com>
2011-09-01 11:32:25 -0400

    Tutorial 4 added

---
+++ Tutorial4/Tutorial4/Tutorial4.cpp
@@ -0,0 +1,15 @@
+// Tutorial4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    cout << "Hello World" << endl;
+    system("PAUSE");
+	return 0;
+}
+
